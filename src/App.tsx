b5843ec0af4b29a0b4c28bb5eb52fac079706c61
Caio Gomes <caio.gomes@telavita.com.br>
2022-12-16 11:28:47 -0300

    refatora App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { RecoilRoot } from 'recoil'
+import { Form } from './components/Form/Form'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <RecoilRoot>
+        <Routes>
+          <Route path="/" element={Form} />
+        </Routes>
+      </RecoilRoot>
+    </BrowserRouter>
+  )
+}
+
+export default App
